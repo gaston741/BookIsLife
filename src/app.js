f9a6a14fd2fb44d7b012f8417ddff56258353302
Luisa <luisa.h.morales@gmail.com>
2022-05-11 09:24:53 -0300

    files moved to deploy in heroku

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const productsRouter = require('./routes/products');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'..' , 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
