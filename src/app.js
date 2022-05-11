b9e0e5f3850f10de401adf8b3db7c90d6013ce73
Luisa <luisa.h.morales@gmail.com>
2022-05-11 10:38:51 -0300

    static fixed

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ const productsRouter = require('./routes/products');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'..' , 'views'));
+app.set('views', path.join(__dirname,  'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 
 /*   RUTAS   */
