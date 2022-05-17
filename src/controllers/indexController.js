a7faa1b033d3fb7846102d61551a7d2023ab331e
Luisa <luisa.h.morales@gmail.com>
2022-05-17 01:09:07 -0300

    products render

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath,'utf-8'))
+
+const toThousand = n => n.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 
 module.exports={
     index: (req, res) =>{
-      return   res.render('index')
+      return   res.render('index',{
+        products
+      })
     } 
 
 
