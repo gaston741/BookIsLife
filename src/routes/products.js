67d3e8884ab292ce0a9703471180e0f010155b0e
Luisa <luisa.h.morales@gmail.com>
2022-05-17 01:09:46 -0300

    methods orgnized

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const{index} = require('../controllers/indexController');
+const {detail,cart,edit,create,store,destroy,update} =require('../controllers/productController')
+ 
+/* Mostrar todos los productos */
+router.get('/',index);
 
-const {detail,cart,edit,create}=require('../controllers/productController')
-/* GET home page. */
-router.get('/detail', detail);
-router.get('/cart', cart);
-router.get('/edit', edit);
+/* Crear un producto  */
 router.get('/create', create);
+router.post('/create', store);
+
+/* Mostrar un producto especifico*/
+router.get('/:id', detail);
+
+/* Editar un producto */
+router.get('/edit', edit);
+router.put('/update', update);
+
+/* Carrito de compras */
+router.get('/cart', cart);
+
+/* Eliminar un producto */
+router.delete('/:id', destroy);
 
 
 
