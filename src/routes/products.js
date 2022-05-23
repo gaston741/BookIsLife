8817ba8c9caf8be758f1a5a395beb77bf95f285f
Luisa <luisa.h.morales@gmail.com>
2022-05-23 15:15:16 -0300

    multer configured

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const path = require ('path')
+//requerimos multer para manipular img files en el form//
+const multer =require('multer');
+
+//requerimos controlador//
 const {index,detail,cart,edit,create,store,destroy,update} =require('../controllers/productController')
  
+//configuracion de multer//
+const storage =multer.diskStorage({
+
+    destination : function( req, file, callback){
+        callback( null, 'public/images/portadas')
+    },
+    filename : function (req,file,callback){
+        callback (null, `${Date.now()}_products_${path.extname(file.originalname)}`)
+
+    }
+})
+const upload = multer({
+    storage
+})
+
+
+
+
+//**************RUTAS********** */
 /* Mostrar  todos los productos*/
 router.get('/',index);
 
 /* Crear un producto  */
 router.get('/create', create);
-router.post('/create', store);
+router.post('/create',upload.single('image'), store);
 
 /* Mostrar un producto especifico*/
 router.get('/detail/:id', detail);
