dfbe2dcc1fdcc6ab87ab645d769136e1ed3d7b0e
Luisa <luisa.h.morales@gmail.com>
2022-05-23 15:46:46 -0300

    multer configured

--- src/routes/products.js
+++ src/routes/products.js
@@ -14,7 +14,7 @@ const storage =multer.diskStorage({
         callback( null, 'public/images/portadas')
     },
     filename : function (req,file,callback){
-        callback (null, `${Date.now()}_products_${path.extname(file.originalname)}`)
+        callback (null, `${Date.now()}_products_${path.extname(file.originalname)}`) // seteo como quiero guardar el nombre original del producto.
 
     }
 })
