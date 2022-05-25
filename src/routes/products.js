3b5b6a8b6058934892c41da3b9e1b95a96776a5b
JuanMaRoldan <juanmanuel.roldan@outlook.com>
2022-05-24 23:25:39 -0300

    config multer

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
 const{index} = require('../controllers/indexController');
 const {detail,cart,edit,create,store,update,destroy} =require('../controllers/productController')
- 
+
+/*MULTER */
+ const storage = multer.diskStorage({
+     destination : (req,file,callback) => {
+         callback(null,'public/images')
+     },
+     filename : (req,file,callback) => {
+         callback(null,'prueba.png')
+     }
+ })
+ const upload = multer({
+     storage
+ })
+
 /* Mostrar todos los productos */
 router.get('/',index);
 
 /* Crear un producto  */
 router.get('/create', create);
-router.post('/create', store);
+router.post('/create', upload.single('image'), store);
 
 /* Mostrar un producto especifico*/
 router.get('/detail/:id', detail);
