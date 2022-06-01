652e0be1a097bba8bdb89f31e117a2c9307706af
JuanMaRoldan <juanmanuel.roldan@outlook.com>
2022-05-31 21:45:58 -0300

    descargando validator

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {register, login} = require ('../controllers/userController')
+const {register, login, processRegister} = require ('../controllers/userController')
 
 /* GET users listing.  /users */
 
 router.get('/register', register);
+router.post('/register', processRegister)
 router.get('/login', login);
 
 
