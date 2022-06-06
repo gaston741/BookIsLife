3c38bf25cc9d5138b53f5ecaa96aa8394cc29a03
Luisa <luisa.h.morales@gmail.com>
2022-06-05 22:23:59 -0300

    working on profile edit

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-//***Controller Require */
-const {register, login, processRegister,processLogin,logout} = require ('../controllers/userController');
+//***UserController Require */
+const {register, login, processRegister,processLogin,logout, profileEdit , updateProfile} = require ('../controllers/userController');
 
 //********Validator require */
 const registerValidator = require('../validations/registerValidator');
 const loginValidator = require('../validations/loginValidator')
-/* GET users listing.  /users */
 
+
+//********Middleware require */
+const checkUser =require('../middlewares/checkUser')
+
+/* GET users listing */
+
+//***to Register Form */
 router.get('/register', register);
 router.post('/register',registerValidator, processRegister);
+
+//***to Login Form */
 router.get('/login', login);
 router.post('/login', loginValidator, processLogin);
-router.get('/logout',logout)
+
+//***to Logout  */
+router.get('/logout',logout);
+
+//***to Profile Form */
+router.get('/profile',checkUser, profileEdit);
+router.put('/update-profile', updateProfile)
 
 
 
