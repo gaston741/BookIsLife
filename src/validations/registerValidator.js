9fdbbc6a8826b9571a7b5209a4325c25579c2f92
Luisa <luisa.h.morales@gmail.com>
2022-06-06 23:21:57 -0300

    resolving conflicts

--- src/validations/registerValidator.js
+++ src/validations/registerValidator.js
@@ -46,9 +46,9 @@ module.exports =[
         .isLength({min: 6 ,max: 12})
         .withMessage("Debe tener un mínimo de 6  y 12 caracteres."),
     
-   check("password")
-        .custom((value,{req})=>{
-            if(value !== req.body.password){
+   check("password2")// validacion de igualdad de contraseñas, campos que estan dentro del formulario (body)
+        .custom((value,{req})=>{ // la validacion custom la usamos cuando ninguna de las validaciones personalizadas se ajusta a lo que queremos validar.
+            if(value !== req.body.password){// si el valor de password es diferente al valor de password 2 = false
 
              return false
             }
@@ -56,25 +56,23 @@ module.exports =[
         })
         .withMessage("Las contraseñas no coinciden"),
     
-    check('avatar')
-        .custom((value, {req}) => {
-        let file = req.file;
-        let extensionsAccepted = [`.jpg` , `.png` , `.gif` ]
-
-        if (!file) {
-            throw new Error ('Tienes que subir una Imagen');
-
-        } else {
-
-            let fileExtension = path.extname(file.originalname);
-        if (!extensionsAccepted.includes(fileExtension)) {
-            throw new Error (`Las extensiones de archivos permitidas son ${extensionsAccepted.join(', ')}`);
+        check('avatar').custom((value, {req}) => {
+            let file = req.file;
+            let extensionsAccepted = ['.jpg' , '.png' , '.gif' ]
+    
+            if (!file) {
+                throw new Error ('Tienes que subir una Imagen');
+    
+            } else {
+    
+                let fileExtension = path.extname(file.originalname);
+            if (!extensionsAccepted.includes(fileExtension)) {
+                throw new Error (`Las extensiones de archivos permitidas son ${extensionsAccepted.join(', ')}`);
+            }
         }
-    }
-
-        return true;
-    }),
-
+    
+            return true;
+        }),
 
     check("terms")
     .isString("on")
