2b194f950e0a1d64b20cb40da666addf777fed62
Luisa <luisa.h.morales@gmail.com>
2022-07-13 13:09:49 -0300

    working on seeds

---
+++ src/database/seeders/20220713021422-genre.js
@@ -0,0 +1,79 @@
+const Sequelize = require('sequelize');
+'use strict';
+
+const genres = [
+  {
+    name: "Autoayuda",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Autobiagrafia",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Clásicos",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Ciencia Ficción",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Cómics y Mangas",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Narrativa",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Narrativa Romantica",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Management",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Obras de Teatro",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Poesía",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Policiales",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: "Terror",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+   
+      await queryInterface.bulkInsert("Genres", genres , {});
+    
+  },
+
+  async down(queryInterface, Sequelize) {
+    
+     await queryInterface.bulkDelete("Genres", null, {});
+    
+  }
+};
