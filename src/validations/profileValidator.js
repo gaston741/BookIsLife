e8335c6469a176b279cb57e1a4725871d0bc4bfc
Adriel Coria <99737740+Adriel3002@users.noreply.github.com>
2022-08-18 09:58:43 -0300

    tratando de Hacer validacion de Perfil

--- src/validations/profileValidator.js
+++ src/validations/profileValidator.js
@@ -1,14 +1,27 @@
+const path = require('path');
 const {check} = require ('express-validator');
 
 module.exports =[
 
-    check("name") //validacion nombre
-    .notEmpty()
-    .withMessage("Campo vacio"),
+    check("name")
+        .isLength({ min: 2 })
+        .withMessage("Minimo 2 caracteres")
+        .bail()
+        .isAlpha()
+        .withMessage("Sólo letras permitidas"),
     
-    check("surname") //validacion apellido
-        .notEmpty()
-        .withMessage("Campo vacio"),
+    check("surname")
+        .isLength({ min: 2 })
+        .withMessage("Mínimo 2 caracteres")
+        .bail()
+        .isAlpha()
+        .withMessage("Sólo letras permitidas"),
 
+    check("tel")
+        .notEmpty()
+        .withMessage("ingresá tu Telefono")
+        .bail()
+        .isNumeric()
+        .withMessage("Sólo números permitidos"),
 
 ]
